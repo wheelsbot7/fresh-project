2a9084c17f076f3211f3a5ea56a8796290c712cd
wheelsbot7 <o.wertzberger@gmail.com>
2024-10-21 21:09:10 -0400

    fmt

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,4 +1,4 @@
 import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
 
-export default defineConfig({plugins: [tailwind()]});
+export default defineConfig({ plugins: [tailwind()] });
